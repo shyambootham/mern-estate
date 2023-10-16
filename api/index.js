2940a7a93909f330861de70102065a11a1cfcee8
BLANK-101 <akshayshanmughanbalan@gmail.com>
2023-10-16 22:16:04 +0530

    mongo db connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Do not encode the entire connection string
+
+mongoose
+  .connect(process.env.Mongo)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
