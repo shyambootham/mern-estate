dd2d59383faf76fcc01c9ff0502f44a26707a607
BLANK-101 <akshayshanmughanbalan@gmail.com>
2023-10-18 18:09:34 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import useRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 // Do not encode the entire connection string
@@ -16,8 +17,19 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("server is running in 3000!!!");
 });
 
 app.use("/api/user", useRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "intenal eror";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
