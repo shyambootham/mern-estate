14ce639990f512f4abd4f12f0acab369d670ad20
BLANK-101 <akshayshanmughanbalan@gmail.com>
2024-01-03 21:12:47 +0530

    complete update listing  functionality page

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 import {
   createListing,
   deleteListing,
+  getListing,
   updateListing,
 } from "../controllers/listing.controller.js";
 
@@ -10,4 +11,5 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 export default router;
