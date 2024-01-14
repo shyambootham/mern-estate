3f73558c75e918e5bc45d35acc8468e246e6ce2f
BLANK-101 <akshayshanmughanbalan@gmail.com>
2024-01-14 23:53:29 +0530

    create listing component and show listings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute.jsx";
 import CreateListing from "./pages/CreateListing.jsx";
 import UpdateListing from "./pages/UpdateListing.jsx";
 import Listing from "./pages/Listing.jsx";
+import Search from "./pages/Search.jsx";
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,7 +20,7 @@ export default function App() {
         <Route path="/Sign-Up" element={<SignUp />} />
         <Route path="/About" element={<About />} />
         <Route path="/listing/:listingId" element={<Listing />} />
-
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path="/Profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,33 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+    console.log(searchTerm);
+    console.log(searchQuery);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    console.log(urlParams);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    console.log(searchTermFromUrl);
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    } else {
+      setSearchTerm("");
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-slate-300 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -12,12 +37,19 @@ export default function Header() {
             <span className="text-slate-700">estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
-            placeholder="search..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="search...."
+            value={searchTerm}
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
